78a01fae7faa6d6119211a53aa6280c76da7e736
akiko.kano <47952174+ichirin-planet@users.noreply.github.com>
2023-08-20 16:58:12 +0900

    lintとか細かな追加設定 (#2)

--- my-app/.eslintrc.js
+++ my-app/.eslintrc.js
@@ -1,20 +1,34 @@
 module.exports = {
-  extends: ["next", "next/core-web-vitals", "prettier","eslint:recommended", "plugin:@typescript-eslint/recommended",],
-  plugins: ["import", "unused-imports"],
+  extends: [
+    'next',
+    'next/core-web-vitals',
+    'eslint:recommended',
+    'plugin:@typescript-eslint/recommended',
+    'plugin:import/warnings',
+    'prettier',
+  ],
+  plugins: ['import', 'unused-imports'],
   rules: {
-    "@typescript-eslint/no-unused-vars": "off",
-    "unused-imports/no-unused-imports": "error", 
-    "import/order": [
-      "error",
+    '@typescript-eslint/no-unused-vars': 'off',
+    'unused-imports/no-unused-imports': 'error',
+    'import/order': [
+      'error',
       {
-        alphabetize: { order: "asc", caseInsensitive: true },
-        groups: ["builtin", "external", "internal", "parent", "sibling", "index", "object", "type"],
-        "newlines-between": "always",
-        pathGroupsExcludedImportTypes: ["builtin"],
-        pathGroups: [
-          { pattern: "*.css", group: "type", position: "after" },
+        alphabetize: { order: 'asc', caseInsensitive: true },
+        groups: [
+          'builtin',
+          'external',
+          'internal',
+          'parent',
+          'sibling',
+          'index',
+          'object',
+          'type',
         ],
-      }
-    ]
+        'newlines-between': 'always',
+        pathGroupsExcludedImportTypes: ['builtin'],
+        pathGroups: [{ pattern: '*.css', group: 'type', position: 'after' }],
+      },
+    ],
   },
-}
+};
